3756b7d18b1e8abcdb83c6066333e8a4d40da41a
shaik Nagur sharif <136973778+Shaik-Sharif@users.noreply.github.com>
2024-05-12 23:31:59 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SubString {
+//	******  Strings are immutable
+	public static void main(String[] args) {
+//		String sentence = "My Name is Safhark Sharif";
+//		String 	name = sentence.substring(11,sentence.length());
+		String name1 = "sharif";
+		String name2 = name1.substring(0);
+//		if your using it you have to give the 0th index with the last index all ill print it
+		System.out.println(name2);
+	}
+}
